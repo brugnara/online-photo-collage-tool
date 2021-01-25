048880a44b21a21f36c3d12b939e0a4cf5b49bcc
Daniele Brugnara <daniele@brugnara.me>
2021-01-25 18:16:56 +0100

    initial

--- go.mod
+++ go.mod
@@ -2,3 +2,8 @@ module github.com/brugnara/online-photo-collage-tool
 
 // +heroku goVersion go1.15
 go 1.15
+
+require (
+	github.com/satori/go.uuid v1.2.0
+	golang.org/x/image v0.0.0-20201208152932-35266b937fa6
+)
--- server.go
+++ server.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"encoding/hex"
+	"fmt"
+	"image"
+	"image/color"
+	_ "image/jpeg"
+	"image/png"
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
+	"golang.org/x/image/draw"
 )
 
 const (
-	bulmaPath = "./public/bulma.min.css"
+	bulmaPath     = "./public/bulma.min.css"
+	fileField     = "file"
+	defaultSize   = 30
+	defaultColor  = "#FFFFFF"
+	defaultHeight = 300
+	maxSSize      = 100
 )
 
 func init() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/css/bulma.min.css", bulmaCSS)
+	http.HandleFunc("/tmp/", imageShow)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 }
 
@@ -29,7 +47,13 @@ func index(w http.ResponseWriter, r *http.Request) {
 		indexPost(w, r)
 		return
 	}
-	tpls.ExecuteTemplate(w, "index.gohtml", nil)
+	tpls.ExecuteTemplate(w, "index.gohtml", struct {
+		FileField     string
+		DefaultSize   int
+		DefaultColor  string
+		DefaultHeight int
+		MaxSSize      int
+	}{fileField, defaultSize, defaultColor, defaultHeight, maxSSize})
 }
 
 func indexPost(w http.ResponseWriter, r *http.Request) {
@@ -39,16 +63,100 @@ func indexPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	key := "file"
+	ssize, err := strconv.Atoi(r.FormValue("ssize"))
+	if err != nil || ssize > maxSSize {
+		ssize = defaultSize
+	}
+	newSize, err := strconv.Atoi(r.FormValue("height"))
+	if err != nil || newSize < ssize*2 {
+		newSize = defaultHeight
+	}
+	scolor := strings.Replace(r.FormValue("scolor")+"FF", "#", "", -1)
+	direction := r.FormValue("direction")
+
+	log.Println(ssize, scolor, direction)
+	// todo:
+	//  - read params
+	//  - read file
+	//  - gen && save image
+	//  - return to visualizer
+
+	validFiles := []multipart.File{}
 
 	if r.MultipartForm != nil && r.MultipartForm.File != nil {
-		if fhs := r.MultipartForm.File[key]; len(fhs) > 0 {
+		for _, file := range r.MultipartForm.File[fileField] {
 			// f, err := fhs[0].Open()
-			log.Println(fhs)
+			f, err := file.Open()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			defer f.Close()
+			validFiles = append(validFiles, f)
 		}
 	}
 
-	io.WriteString(w, "ciao")
+	log.Println(validFiles)
+
+	// gen image based on validFiles len
+	x := len(validFiles)*(ssize+newSize) + ssize
+	y := newSize + 2*ssize
+	if direction == "v" {
+		x, y = y, x
+	}
+	log.Println("Image size:", x, y)
+	img := image.NewRGBA(image.Rect(0, 0, x, y))
+	// todo: fill image with scolor
+	// leave if wanted transparent
+	b, err := hex.DecodeString(scolor)
+	if err != nil {
+		log.Fatal(err)
+	}
+	clr := color.RGBA{b[0], b[1], b[2], 0}
+	//
+	for i := 0; i < x; i++ {
+		for j := 0; j < y; j++ {
+			img.Set(i, j, clr)
+		}
+	}
+	//
+	log.Printf("%T\n", img)
+
+	// todo: allow user to select scaler based on quality
+	scaler := draw.NearestNeighbor
+
+	// https://pkg.go.dev/golang.org/x/image/draw#Scaler
+	for i, f := range validFiles {
+		src, _, err := image.Decode(f)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		// todo: flip based on direction
+		x0 := i*(newSize+ssize) + ssize
+		y0 := ssize
+		x1 := x0 + newSize
+		y1 := y0 + newSize
+		dr := image.Rect(x0, y0, x1, y1)
+		fmt.Println(dr, src.Bounds())
+		scaler.Scale(img, dr, src, src.Bounds(), draw.Over, nil)
+	}
+
+	// open file to save
+	fileName := fmt.Sprintf("./tmp/%s.png", uuid.NewV4())
+	dstFile, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dstFile.Close()
+	// encode as .png to the file
+
+	err = png.Encode(dstFile, img)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tpls.ExecuteTemplate(w, "img.gohtml", fileName)
 }
 
 func bulmaCSS(w http.ResponseWriter, r *http.Request) {
@@ -58,5 +166,15 @@ func bulmaCSS(w http.ResponseWriter, r *http.Request) {
 	}
 	defer f.Close()
 	w.Header().Set("content-type", "text/css")
+	io.Copy(w, f)
+}
+
+func imageShow(w http.ResponseWriter, r *http.Request) {
+	log.Println("showing image:", r.URL.Path)
+	f, err := os.Open("./" + r.URL.Path)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer f.Close()
 	io.Copy(w, f)
 }
